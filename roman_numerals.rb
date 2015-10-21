fa291acd2c7ff9ea61e33c7d50a125daff1691a3
ZeroLive <zerolive@gmail.com>
2015-10-21 18:19:28 +0200

    Some refactor

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,26 +1,29 @@
 class RomanCitizen
 
-	LETTERS = { 1000 => "M", 900 => "CM", 500 => "D", 400 => "CD", 100 => "C", 90 => "XC", 50 => "L", 40 => "XL", 10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I"}
+	LETTERS = { 1000 => "M", 900 => "CM", 500 => "D", 400 => "CD", 100 => "C", 90 => "XC", 
+							50 => "L", 40 => "XL", 10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I"}
 
-	def self.translate number		
-		textreturn = ""
-		count = 0
-		while number > 0
-			quantity = number / LETTERS.keys[count]
-			textreturn << LETTERS.values[count] * quantity
-			number = number % LETTERS.keys[count]
-			count += 1
+	class << self
+
+		def translate number		
+			textreturn = ""
+			LETTERS.each { |key, value| 
+				textreturn << roman_number(number, key, value)
+				number = next_number(number, key)				
+			}
+			return textreturn
 		end
-		return textreturn
+
+		private
+
+			def roman_number number, key, value
+				quantity = number / key
+				return value * quantity
+			end
+
+			def next_number number, key
+				return number % key
+			end
+
 	end
 end
-
- #I = 1
- #V = 5
- #X = 10
- #L = 50
- #C = 100
- #D = 500
- #M = 1,000
-
- #p RomanCitizen.translate(1238)
--- testromannumerals.rb
+++ testromannumerals.rb
@@ -1,33 +1,41 @@
 class RomanCitizen
 
-	LETTERS = { 1000 => "M", 900 => "CM", 500 => "D", 400 => "CD", 100 => "C", 90 => "XC", 50 => "L", 40 => "XL", 10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I"}
+	LETTERS = { 1000 => "M", 
+							900 => "CM", 500 => "D", 400 => "CD", 100 => "C", 
+							90 => "XC", 50 => "L", 40 => "XL", 10 => "X", 
+							9 => "IX", 5 => "V", 4 => "IV", 1 => "I"}
 
 	class << self
 
 		def translate number		
-			translate = ""
-			count = 0
-			while number > 0
-				rest = number % LETTERS.keys[count]
-				number = number / LETTERS.keys[count]
-				translate << LETTERS.values[count] * number
-				count += 1
-				number = rest
-			end
-			return translate
+			textreturn = ""
+			LETTERS.each { |key, value| 
+				textreturn << roman_number(number, key, value)
+				number = next_number(number, key)				
+			}
+			return textreturn
 		end
 
 		private
 
+			def roman_number number, key, value
+				quantity = number / key
+				return value * quantity
+			end
+
+			def next_number number, key
+				return number % key
+			end
+
 	end
 end
 
- #I = 1
- #V = 5
- #X = 10
- #L = 50
- #C = 100
- #D = 500
- #M = 1,000
+#I = 1
+#V = 5
+#X = 10
+#L = 50
+#C = 100
+#D = 500
+#M = 1,000
 
- p RomanCitizen.translate(1238)
+p RomanCitizen.translate(1238)
