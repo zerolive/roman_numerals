3f2f5a2db022d89a37e55f12b8c2a091d982302a
ZeroLive <zerolive@gmail.com>
2015-10-21 12:34:22 +0200

    RomanCitizen translate number

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,6 +1,35 @@
 class RomanCitizen
 
-	def self.translate number
-		return "I"
+	class << self
+
+		def translate number
+			textreturn = ""
+			textreturn << "X" if compare_number_roman(number, 10)
+			textreturn << "IX" if compare_number_roman(number, 9)
+			textreturn << "VIII" if compare_number_roman(number, 8)
+			textreturn << "VII" if compare_number_roman(number, 7)
+			textreturn << "VI" if compare_number_roman(number, 6)
+			textreturn << "V" if compare_number_roman(number, 5)
+			textreturn << "IV" if compare_number_roman(number, 4)
+			textreturn << "III" if compare_number_roman(number, 3)
+			textreturn << "II" if compare_number_roman(number, 2)
+			textreturn << "I" if compare_number_roman(number, 1)
+			return textreturn
+		end
+
+		def compare_number_roman number, romannumber
+			(number - romannumber).zero?
+		end
+
 	end
 end
+
+ #I = 1
+ #V = 5
+ #X = 10
+ #L = 50
+ #C = 100
+ #D = 500
+ #M = 1000
+
+ p RomanCitizen.translate(9)
--- spec/romannumerals_spec.rb
+++ spec/romannumerals_spec.rb
@@ -6,31 +6,31 @@ describe "A roman citizen" do
 	it "translates 1 to I" do
 		expect(RomanCitizen.translate(1)).to eq("I")
 	end
-	xit "translates 2 to II" do
+	it "translates 2 to II" do
 		expect(RomanCitizen.translate(2)).to eq("II")
 	end
-	xit "translates 3 to III" do
+	it "translates 3 to III" do
 		expect(RomanCitizen.translate(3)).to eq("III")
 	end
-	xit "translates 4 to IV" do
+	it "translates 4 to IV" do
 		expect(RomanCitizen.translate(4)).to eq("IV")
 	end
-	xit "translates 5 to V" do
+	it "translates 5 to V" do
 		expect(RomanCitizen.translate(5)).to eq("V")
 	end
-	xit "translates 6 to VI" do
+	it "translates 6 to VI" do
 		expect(RomanCitizen.translate(6)).to eq("VI")
 	end
-	xit "translates 7 to VII" do
+	it "translates 7 to VII" do
 		expect(RomanCitizen.translate(7)).to eq("VII")
 	end
-	xit "translates 8 to VIII" do
+	it "translates 8 to VIII" do
 		expect(RomanCitizen.translate(8)).to eq("VIII")
 	end
-	xit "translates 9 to IX" do
+	it "translates 9 to IX" do
 		expect(RomanCitizen.translate(9)).to eq("IX")
 	end
-	xit "translates 10 to X" do
+	it "translates 10 to X" do
 		expect(RomanCitizen.translate(10)).to eq("X")
 	end
 end
