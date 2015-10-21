cec3211e23d9419df0d8caf914e4f43e9033adf1
ZeroLive <zerolive@gmail.com>
2015-10-21 15:45:00 +0200

    RomanCitizen does any number

--- testromannumerals.rb
+++ testromannumerals.rb
@@ -3,60 +3,60 @@ class RomanCitizen
 	class << self
 
 		def translate number
-			texttoreturn = ""
+			translate = ""
 			while number >= 1000
-				texttoreturn << "M"
+				translate << "M"
 				number -= 1000
 			end
 			while number >= 900
-				texttoreturn << "CM"
+				translate << "CM"
 				number -= 900
 			end
 			while number >= 500
-				texttoreturn << "D"
+				translate << "D"
 				number -= 500
 			end
 			while number >= 400
-				texttoreturn << "CD"
+				translate << "CD"
 				number -= 400
 			end
 			while number >= 100
-				texttoreturn << "C"
+				translate << "C"
 				number -= 100
 			end
 			while number >= 90
-				texttoreturn << "XC"
+				translate << "XC"
 				number -= 90
 			end
 			while number >= 50
-				texttoreturn << "L"
+				translate << "L"
 				number -= 50
 			end
 			while number >= 40
-				texttoreturn << "XL"
+				translate << "XL"
 				number -= 40
 			end
 			while number >= 10
-				texttoreturn << "X"
+				translate << "X"
 				number -= 10
 			end
 			while number >= 9
-				texttoreturn << "IX"
+				translate << "IX"
 				number -= 9
 			end
 			while number >= 5
-				texttoreturn << "V"
+				translate << "V"
 				number -= 5
 			end
 			while number >= 4
-				texttoreturn << "IV"
+				translate << "IV"
 				number -= 4
 			end
 			while number >= 1
-				texttoreturn << "I"
+				translate << "I"
 				number -= 1
 			end
-			return texttoreturn
+			return translate
 		end
 
 		private
