8ef2e15cde0f259f43305dc59dc476fbf1d67f5a
ZeroLive <zerolive@gmail.com>
2015-10-21 14:21:40 +0200

    RomanCitizen knows translate until 39

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -4,7 +4,16 @@ class RomanCitizen
 
 		def translate number
 			romannumbers = ["0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"]
-			return romannumbers[number]
+			translated = ""
+			position = 10
+			while position > 0
+				if number / position > 0
+					translated << (romannumbers[position]*(number/position))
+					number = number % position
+				end
+				position -= 1
+			end
+			return translated
 		end
 
 		private
@@ -15,3 +24,5 @@ end
  #I = 1
  #V = 5
  #X = 10
+
+ #p RomanCitizen.translate(39)
